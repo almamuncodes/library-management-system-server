c30a7bf48652fe538321021eb623666491395ca4
MAlmamunislam <almamun2026islam@gmail.com>
2026-09-15 01:10:00 +0600

    fixed

--- src/config/db.js
+++ src/config/db.js
@@ -9,7 +9,7 @@ try {
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-  const uri = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/library_management';
+  const uri = process.env.MONGODB_URI ;
   try {
     const conn = await mongoose.connect(uri, {
       serverSelectionTimeoutMS: 5000,
